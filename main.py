207a3a03393a1d9cdcc370b1c1d95fc3d2862cf9
Ben Tatman <ben@tatmans.co.uk>
2016-07-09 10:28:11 +0100

    Made it so dying prey and predators contribute to food

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import random
 import math
-random.seed(1)
+random.seed(2)
 
 producers = 10000
 prey = 100
 predators = 1
 
-repeats = 100
+repeats = 1000
 
 for i in range(0, repeats):
     print(str(producers) + ", " + str(prey) + ", " + str(predators))
     new_producers = 0
     new_prey = 0
     new_predators = 0
-#    for m in range(0, producers):
-#        if (random.randint(1, 3) >= 2):
-#            new_producers = new_producers + 2
-#        else:
-#            new_producers = new_producers + 1
-    new_producers = producers + (90*(1+math.sin(i * (math.pi/40))))
+
+    #new_producers = producers + 150 # Constant growth
+    new_producers = producers + (150*(1+math.sin(i * (math.pi/40)))) # Seasons
 
 
     for n in range(0, prey):
@@ -37,6 +34,7 @@ for i in range(0, repeats):
         elif (q_score <= 10 and q_score > 1):
             if (rand_score < 3):
                 # Dies, does not pass on.
+                new_producers = new_producers + 0.5
                 new_prey = new_prey
             elif (rand_score > 7):
                 # Reproduces
@@ -51,6 +49,8 @@ for i in range(0, repeats):
                 new_prey = new_prey + 1
                 new_producers = new_producers - 1
             else:
+                # Dies
+                new_producers = new_producers + 0.5
                 new_prey = new_prey
 
     for p in range(0, predators):
@@ -70,6 +70,7 @@ for i in range(0, repeats):
             if (rand_score < 3):
                 # Dies, does not pass on.
                 new_predators = new_predators
+                new_producers = new_producers + 0.3
             elif (rand_score > 7):
                 # Reproduces
                 new_predators = new_predators + 2
@@ -84,8 +85,10 @@ for i in range(0, repeats):
                 new_prey = new_prey - 1
             else:
                 new_predators = new_predators
+                new_producers = new_producers + 0.3
         else:
             new_predators = new_predators
+            new_producers = new_producers + 0.3
     #new_producers = producers
     prey = new_prey
     predators = new_predators
